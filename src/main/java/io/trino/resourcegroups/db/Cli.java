cf1ab7c40a5670adb10175c53e4156b601add2b3
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2022-02-03 11:40:10 -0500

    Add apache license

--- src/main/java/io/trino/resourcegroups/db/Cli.java
+++ src/main/java/io/trino/resourcegroups/db/Cli.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.trino.resourcegroups.db;
 
 import com.google.common.collect.ImmutableList;
@@ -10,7 +23,6 @@ import io.trino.plugin.resourcegroups.ManagerSpec;
 import io.trino.plugin.resourcegroups.ResourceGroupIdTemplate;
 import io.trino.plugin.resourcegroups.ResourceGroupSpec;
 import io.trino.plugin.resourcegroups.SelectorSpec;
-import io.trino.plugin.resourcegroups.db.DbResourceGroupConfig;
 import picocli.CommandLine;
 
 import static com.google.common.base.Throwables.throwIfUnchecked;
